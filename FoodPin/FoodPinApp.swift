fb406e41944c9a3dd92e876a4ce3dec8984a8731
Divyansh Kaushik <kaushik.divyansh@icloud.com>
2022-03-28 14:48:17 -0500

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Divyansh Kaushik on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
